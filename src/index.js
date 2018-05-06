e3792f4b49b6c9e415dc20d5850af4664db4aa5e
Tania Akter <taniatani51@gmail.com>
2018-05-06 15:46:54 +0600

    function and class in tag

--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-let me = 'TT'
-let nameObj = {
-  fName: 'Harry',
-  lName: 'Poter'
-}
-const test = <h1>Hello {nameObj.fName} {nameObj.lName}! This is {me}</h1>
+// let me = 'TT'
+// let nameObj = {
+//   fName: 'Harry',
+//   lName: 'Poter'
+// }
+// const test = <h1>Hello {nameObj.fName} {nameObj.lName}! This is {me}</h1>
 
+//function
+
+function test(place ='Zootopia', car = 'flying'){
+  return <div>
+          <h2>She wanna go somewhere like {place} by {car} . </h2>
+          <p className = "test_msg">Can you hear me?!!</p>
+        </div>
+}
 ReactDOM.render(
-  test,
+  test("Cox's Bazar" , "train"),
   document.getElementById('root')
 );
