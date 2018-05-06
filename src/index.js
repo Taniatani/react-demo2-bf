4ee3c8f858a519bc9e7d22c567103dc9f5904579
Tania Akter <taniatani51@gmail.com>
2018-05-06 15:24:58 +0600

    testing in index

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+let me = 'TT'
+let nameObj = {
+  fName: 'Harry',
+  lName: 'Poter'
+}
+const test = <h1>Hello {nameObj.fName} {nameObj.lName}! This is {me}</h1>
+
 ReactDOM.render(
-  <h1>Hello there!</h1>,
+  test,
   document.getElementById('root')
 );
