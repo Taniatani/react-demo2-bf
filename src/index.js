8f5906bf77e4e83da92241dc552e04bc9d707817
Tania Akter <taniatani51@gmail.com>
2018-05-06 16:06:07 +0600

    tuto-6: JSX

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import ReactDOM from 'react-dom'
 // const test = <h1>Hello {nameObj.fName} {nameObj.lName}! This is {me}</h1>
 
 //function
-
+ //Tuto : 6
 function test(place ='Zootopia', car = 'flying'){
   return <div>
           <h2>She wanna go somewhere like {place} by {car} . </h2>
