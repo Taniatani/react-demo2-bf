024954c3da3dd1d9ee707087c6109301b1a5c0ec
Tania Akter <taniatani51@gmail.com>
2018-05-07 17:42:07 +0600

    Event handling/Increment app

--- src/index.js
+++ src/index.js
@@ -1,115 +1,75 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-// let me = 'TT'
-// let nameObj = {
-//   fName: 'Harry',
-//   lName: 'Poter'
-// }
-// const test = <h1>Hello {nameObj.fName} {nameObj.lName}! This is {me}</h1>
-
-//function
- //Tuto : 6
-// function test(place ='Zootopia', car = 'flying'){
-//   return <div>
-//           <h2>She wanna go somewhere like {place} by {car} . </h2>
-//           <p className = "test_msg">Can you hear me?!!</p>
-//         </div>
-// }
-// ReactDOM.render(
-//   test("Cox's Bazar" , "train"),
-//   document.getElementById('root')
-// );
-
-
- //Tuto : 7 - clock app
-
-// let clock = function () {
-//   return <h2>Current time is {new Date().toLocaleTimeString()}</h2>
-// }
+  //1st way
+// class Inc extends React.Component {
 //
-// setInterval (function(){
-//  ReactDOM.render(
-//    clock(),
-//    document.getElementById('root')
-//  );
-// },1000);
-
- //Tuto : 8 - Comps and props
-//component by using function
-// function Place(props){
-//   return<h2>She wanna go somewhere like {props.name}.</h2>
-// }
-
-//component by using class
-// class Place extends React.Component{
-//   render() {
-//     return<h2>She wanna go somewhere like {this.props.name}.</h2>
+//   constructor(props) {
+//     super(props);
+//     this.state = {counter : 0}
+//     this.increment = this.increment.bind(this);
+//   }
+//
+//   increment(e){
+//     e.preventDefault();
+//     this.setState({
+//       counter: this.state.counter + 1
+//     })
+//   }
+//
+//   render(){
+//     return(
+//       <a href = 'http://google.com' onClick = {this.increment}> Value is {this.state.counter}</a>
+//     )
 //   }
 // }
 
 
-// component in another component
-// function Place(props){
-//   return<h2>She wanna go somewhere like {props.name} by {props.car}</h2>
-//  }
+//2nd way
+//   class Inc extends React.Component {
 //
-//  function AllPlace(props){
-//    return<div>
-//     <Place name = 'Island' car = 'air'/>
-//     <Place name = 'Sea' car = 'Flying'/>
-//    </div>
+//   constructor(props) {
+//     super(props);
+//     this.state = {counter : 0}
 //   }
 //
-// ReactDOM.render(
-//   <AllPlace/>,
-//   document.getElementById('root')
+//   increment(e){
+//     e.preventDefault();
+//     this.setState({
+//       counter: this.state.counter + 1
+//     })
+//   }
 //
-// );
+//   render(){
+//     return(
+//       <a href = 'http://google.com' onClick = {(e) => this.increment(e)}> Value is {this.state.counter}</a>
+//     )
+//   }
+// }
 
- //Tuto : 9 - States and lifecycle
+//3rd way
+  class Inc extends React.Component {
 
-
-// function Clock() {
-//    return <h2>Current time is {new Date().toLocaleTimeString()}</h2>
-//  }
-
-class Clock extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state = {
-      date : new Date()
-    }
+    this.state = {counter : 0}
   }
 
-  componentDidMount(){
-    this.timer = setInterval( () => this.start(), 1000);
-  }
-
-//for removing timer
-
-   componentWillUnmount(){
-     clearInterval(this.timer);
-   }
-
-  start(){
+  increment = (e) => {
+    e.preventDefault();
     this.setState({
-      date: new Date()
-    });
+      counter: this.state.counter + 1
+    })
   }
 
-  render() {
-    return <h2>Current time is {this.state.date.toLocaleTimeString()}</h2>
+  render(){
+    return(
+      <button onClick = {this.increment}> Value is {this.state.counter}</button>
+    )
   }
 }
 
-  ReactDOM.render(
-    <Clock/>,
-    document.getElementById('root')
-  );
-
- // setInterval (function(){
- // time();
- // },1000);
-
- //setInterval (time,1000);
+ReactDOM.render(
+  <Inc/>,
+  document.getElementById('root')
+)
---
+++ src/index9.js
@@ -0,0 +1,117 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+// let me = 'TT'
+// let nameObj = {
+//   fName: 'Harry',
+//   lName: 'Poter'
+// }
+// const test = <h1>Hello {nameObj.fName} {nameObj.lName}! This is {me}</h1>
+
+//function
+ //Tuto : 6
+// function test(place ='Zootopia', car = 'flying'){
+//   return <div>
+//           <h2>She wanna go somewhere like {place} by {car} . </h2>
+//           <p className = "test_msg">Can you hear me?!!</p>
+//         </div>
+// }
+// ReactDOM.render(
+//   test("Cox's Bazar" , "train"),
+//   document.getElementById('root')
+// );
+
+
+ //Tuto : 7 - clock app
+
+// let clock = function () {
+//   return <h2>Current time is {new Date().toLocaleTimeString()}</h2>
+// }
+//
+// setInterval (function(){
+//  ReactDOM.render(
+//    clock(),
+//    document.getElementById('root')
+//  );
+// },1000);
+
+ //Tuto : 8 - Comps and props
+//component by using function
+// function Place(props){
+//   return<h2>She wanna go somewhere like {props.name}.</h2>
+// }
+
+//component by using class
+// class Place extends React.Component{
+//   render() {
+//     return<h2>She wanna go somewhere like {this.props.name}.</h2>
+//   }
+// }
+
+
+// component in another component
+// function Place(props){
+//   return<h2>She wanna go somewhere like {props.name} by {props.car}</h2>
+//  }
+//
+//  function AllPlace(props){
+//    return<div>
+//     <Place name = 'Island' car = 'air'/>
+//     <Place name = 'Sea' car = 'Flying'/>
+//    </div>
+//   }
+//
+// ReactDOM.render(
+//   <AllPlace/>,
+//   document.getElementById('root')
+//
+// );
+
+ //Tuto : 9 - States and lifecycle
+
+
+// function Clock() {
+//    return <h2>Current time is {new Date().toLocaleTimeString()}</h2>
+//  }
+
+class Clock extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      date : new Date()
+    }
+  }
+
+  componentDidMount(){
+    this.timer = setInterval( () => this.start(), 1000);
+  }
+
+//for removing timer
+
+   componentWillUnmount(){
+     clearInterval(this.timer);
+   }
+
+  start(){
+    this.setState({
+      date: new Date()
+    });
+  }
+
+  render() {
+    return (
+      <h2>Current time is {this.state.date.toLocaleTimeString()}</h2>
+    );
+  }
+}
+
+  ReactDOM.render(
+    <Clock/>,
+    document.getElementById('root')
+  );
+
+ // setInterval (function(){
+ // time();
+ // },1000);
+
+ //setInterval (time,1000);
